7f290fbae78dd625088bdc00bafc92332b9c35e6
Daniel Hughes <daniel.hughes.sweden@gmail.com>
2020-01-18 16:34:12 +0100

    Added some SQL

--- src/SQL_Files/sko-webshop Functions.sql
+++ src/SQL_Files/sko-webshop Functions.sql
@@ -1,3 +1,5 @@
+
+-- Function (boolean): shoe exists in database
 USE `webshop_skor`;
 DROP function IF EXISTS `is_shoe_in_DB`;
 
@@ -7,7 +9,48 @@ CREATE FUNCTION `is_shoe_in_DB`(shoe_id INT)
 RETURNS BOOLEAN
 BEGIN
 DECLARE nr_found INT;
-SELECT COUNT(*) FROM sko WHERE sko_id = shoe_id INTO nr_found;
+SELECT COUNT(*) INTO nr_found FROM sko WHERE sko_id = shoe_id;
+IF nr_found = 0 THEN
+	RETURN FALSE;
+ELSE
+	RETURN TRUE;
+END IF;
+END$$
+
+DELIMITER ;
+
+-- Function (boolean): customer exists in database
+USE `webshop_skor`;
+DROP function IF EXISTS `is_customer_in_DB`;
+
+DELIMITER $$
+USE `webshop_skor`$$
+CREATE FUNCTION `is_customer_in_DB`(customer_id INT)
+RETURNS BOOLEAN
+BEGIN
+DECLARE nr_found INT;
+SELECT COUNT(*) INTO nr_found FROM kund WHERE kund_id = customer_id;
+IF nr_found = 0 THEN
+	RETURN FALSE;
+ELSE
+	RETURN TRUE;
+END IF;
+END$$
+
+DELIMITER ;
+
+
+-- Function (boolean): order exists in database
+USE `webshop_skor`;
+DROP function IF EXISTS `is_order_in_DB`;
+
+DELIMITER $$
+USE `webshop_skor`$$
+CREATE FUNCTION `is_order_in_DB`(order_id INT)
+RETURNS BOOLEAN
+BEGIN
+DECLARE nr_found INT;
+SELECT COUNT(*) INTO nr_found FROM beställning WHERE beställning_id = order_id;
 IF nr_found = 0 THEN
 	RETURN FALSE;
 ELSE
--- src/SQL_Files/sko-webshop SP.sql
+++ src/SQL_Files/sko-webshop SP.sql
@@ -1,10 +1,22 @@
 USE webshop_skor;
+DROP PROCEDURE IF EXISTS add_to_cart;
 
 DELIMITER //
-CREATE PROCEDURE add_to_cart(IN kund INT, IN beställning INT, IN sko INT)
+CREATE PROCEDURE add_to_cart(IN customer_id INT, IN order_id INT, IN shoe_id INT)
 BEGIN
-
-
+IF NOT is_shoe_in_DB(shoe_id) OR NOT is_customer_in_DB(customer_id) THEN
+	-- throw error
+    LEAVE sp;
+ELSE
+	IF NOT is_order_in_DB(order_id) OR order_id IS NULL THEN
+		INSERT INTO beställning (kund_id, datum) VALUES (customer_id, CURRENT_DATE());
+        INSERT INTO beställningsvara (beställning_id, sko_id, antal) VALUES(LAST_INSERT_ID(), shoe_id, 1);
+	ELSE -- order exists
+		
+        -- If order_item doesn't exist
+		INSERT INTO beställningsvara (beställning_id, sko_id, antal) VALUES(order_id, shoe_id, 1);
+	END IF;
+END IF; 
 
 END //
 
